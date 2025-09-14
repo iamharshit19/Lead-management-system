52cff32a48a30486e37d712d34398cc9620ebb0c
iamharshit19 <harshitgupta0901@gmail.com>
2025-09-14 12:04:18 +0530

    deployment issue

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", 
+  baseURL: "https://lead-management-system-jsmm.onrender.com", 
   withCredentials: true 
 });
 
